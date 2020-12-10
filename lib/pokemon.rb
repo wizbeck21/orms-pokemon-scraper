fc7a6ad98a288964ad161ded9185e8c5797ebe4c
Wesley <wes.beck21@outlook.com>
2020-12-10 13:35:12 -0800

    commit tree 1cef6e4dc12b73694cf842665eff83f3a256d558

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+attr_accessor :id, :name, :type, :db
+
+def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+end
+
+def self.save(name, type, db)
+sql = <<-SQL
+INSERT INTO pokemon (name, type) VALUES (?, ?);
+SQL
+db.execute(sql, name, type)
+end
+
+def self.find(poke_id, db)
+sql = <<-SQL
+SELECT * 
+FROM pokemon
+WHERE id = ?
+LIMIT 1
+SQL
+new_pokemon = db.execute(sql, poke_id).flatten
+Pokemon.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], db: db)
+end
+
+
 end
